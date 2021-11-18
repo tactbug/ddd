364c15e94d34e1be65c6a4484015073502dd2cb7
tactbug <tactbug@gmail.com>
2021-11-18 22:01:59 +0800

    [building] CQRS building

--- query/src/main/java/com/tactbug/query/replay/EventRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tactbug.query.replay;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRecordRepository extends MongoRepository<EventRecord, Long> {
-
-}
--- query/src/main/java/com/tactbug/query/replay/product/repository/CategoryDTORepository.java
+++
@@ -1,9 +0,0 @@
-package com.tactbug.query.replay.product.repository;
-
-import com.tactbug.query.dto.product.CategoryDTO;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryDTORepository extends MongoRepository<CategoryDTO, Long> {
-}
